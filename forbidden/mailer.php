66a362a852202b740a14563fb674a451ac65a725
Liam Andersson <33359876+Dubstepzedd@users.noreply.github.com>
2023-01-14 16:05:41 +0100

    Fixed issue

--- forbidden/mailer.php
+++ forbidden/mailer.php
@@ -15,18 +15,18 @@
         $mail = new PHPMailer(true);
 
         try {
-            //Server settings
+            //Server settings [Change these as they are invalid]
             //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = "websmtp.simply.com";                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = "no-reply@feliciastudent.se";                     //SMTP username
-            $mail->Password   = "Liam1234";                               //SMTP password
+            $mail->Username   = "XXXXXXXXX";                     //SMTP username
+            $mail->Password   = "********";                                //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
             $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
             $mail->CharSet = 'UTF-8';
             //Recipients
-            $mail->setFrom("no-reply@feliciastudent.se");
+            $mail->setFrom("XXXXXXXXX");
             $mail->addAddress($email);     //Add a recipient
 
             //Content 
